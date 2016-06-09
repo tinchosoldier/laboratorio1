f64c7a39e62025ae16f4fc6b5906efde8cadcc51
MartinS <tinchosoldier@gmail.com>
2016-06-08 23:02:52 -0300

    Cambios Oferta

--- obligatorio5/Oferta.h
+++ obligatorio5/Oferta.h
@@ -5,12 +5,7 @@
 #include <map>
 #include <vector>
 #include "DtFecha.h"
-//#include "Asignatura.h"
-//#include "DtAsignatura.h"
-//#include "IObserver.h"
-//#include "DtEstudiante.h"
 #include "DtOferta.h"
-//#include "Estudiante.h"
 #include "Oferta.h"
 #include "Seccion.h"
 
@@ -31,45 +26,11 @@ class Oferta{
 	DtFecha fechaFin;
 	int cantPuestos;
 	Seccion* seccion;
-	map<string,Estudiante*> colEstudiantesInscriptos;
-	map<int,Asignatura*> colAsignaturasRequeridas;
-	map<string,DtFecha> entrevistas;
 */
     public:
 
         Oferta();
-/*	Oferta(const Oferta&);
-	Oferta(int, string, string, int, float, float,DtFecha,DtFecha,int,map<int,Asignatura*>,Seccion*);
 
-	int getNumExpediente() const;
-	string getTitulo() const;
-	string getDescripcion() const;
-	int getCantHorasSemanales() const;
-	float getSalarioMin() const;
-	float getSalarioMax() const;
-	DtFecha getFechaInicio() const;
-	DtFecha getFechaFin() const;
-	int getCantPuestos() const;
-	void setNumExpediente(int);
-	void setTitulo(string);
-	void setDescripcion(string);
-	void setCantHorasSemanales(int);
-	void setSalarioMin(float);
-	void setSalarioMax(float);
-	void setFechaInicio(DtFecha);
-	void setFechaFin(DtFecha);
-	void setCantPuestos(int);
-
-        DtOferta getDtOferta();
-        void agregarEstudiante(Estudiante*);
-        void agregarAsignatura(Asignatura*);
-        bool noTieneEstudiantes();
-        bool existeEstudiante(string);
-        vector<DtEstudiante>getSetDtEstudiante();
-        vector<DtAsignatura>getSetDtAsignatura();
-        Seccion* getSeccion();
-        void agregarEstudianteEntrevista(string,DtFecha);
-*/
 	virtual ~Oferta();
 };
 
