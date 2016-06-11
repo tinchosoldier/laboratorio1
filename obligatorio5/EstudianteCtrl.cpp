847fd97d05d62c13148969e2d7012cda8e3b96c1
MartinS <tinchosoldier@gmail.com>
2016-06-11 17:42:27 -0300

    EstudianteCtrl Successful

---
+++ obligatorio5/EstudianteCtrl.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "EstudianteCtrl.h"
+#include "Asignatura.h"
+#include "DtAsignatura.h"
+#include "DtOferta.h"
+#include "Oferta.h"
+
+using namespace std;
+
+EstudianteCtrl::EstudianteCtrl(){}
+
+vector<DtOferta> EstudianteCtrl::listarOfertasActivas(){}
+        
+void EstudianteCtrl::confirmarInscripcion(DtEstudiante dte){}
+
+vector<DtEstudiante> EstudianteCtrl::listarEstudiantes(){}
+
+DtEstudiante EstudianteCtrl::listarEstudiante(string ci){}
+
+EstudianteCtrl::~EstudianteCtrl(){}
